14a6bcbe5c585a2ce1510ab5e012c5f1504249fe
nicholas.castaneda <nicholas.castaneda@bluetab.net>
2021-03-09 17:14:55 -0500

    how does while work

---
+++ Clase5/bin/while_do_while.dart
@@ -0,0 +1,12 @@
+main(List<String> arguments){
+  int i =0;
+  while(i <= 10){
+    print('valor actual $i');
+    i++;
+  }
+  i =1;
+  do {
+    print('valor actual 2 $i');
+    i++;
+  } while(i <=10);
+}
